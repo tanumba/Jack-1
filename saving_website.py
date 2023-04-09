7d028a927a2b761dcab8bbfc0c8d5b7301eeb65b
tanumba <129976211+tanumba@users.noreply.github.com>
2023-04-09 22:57:12 +0300

    Add files via upload

---
+++ saving_website.py
@@ -0,0 +1,11 @@
+from pywebcopy import save_website
+save_website(
+    url="https://1470umto.ru/",
+    project_folder="E://savedpages//",
+    project_name="my_site",
+    bypass_robots=True,
+    debug=True,
+    open_in_browser=True,
+    delay=None,
+    threaded=False,
+)
